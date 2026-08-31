1e01f65b87cb3e2f69b629e15ab20f8b0f7dee85
Kalp Doshi <kalpdoshi999@gmail.com>
2026-08-31 18:41:13 +0530

    Day 1 Completed - Built Band Name Generator

---
+++ Day-001-Band_Name_Gnerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to Band Name Generator!")
+
+which_city = input("Which City you grew up? \n")
+pet_name = input("What is your pet name? \n")
+
+band_name = which_city + " " + pet_name
+print(f"Your band name could be : {band_name}")
